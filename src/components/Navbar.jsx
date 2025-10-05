9b6c1a984a1a5ad786efa21f2fe445131e6f0e45
alasaneeth <alasaneeth22@gmail.com>
2025-10-05 14:21:05 +0300

    Feat: Implement Search Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,13 @@ const Navbar = () => {
             
            })} 
            
-        
+           <div className='hidden lg:flex items-center text-sm gap-2 border 
+            border-borderColor px-3 rounded-full max-w-56'>
+                <input type='text' className='py-1.5 w-full bg-transparent 
+                 outline-none placeholder-gray-500' placeholder="Search Products"></input>
+                 <img src={assets.search_icon} alt='search'/>           
+                 
+            </div>
         </div>
     </div>
   )
