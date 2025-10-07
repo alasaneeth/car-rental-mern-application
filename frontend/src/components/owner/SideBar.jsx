a49e5775fa21d633d81e3b1835804075bd648c05
alasaneeth <alasaneeth22@gmail.com>
2025-10-07 10:20:29 +0300

    Feat: Designed Side Bar

--- frontend/src/components/owner/SideBar.jsx
+++ frontend/src/components/owner/SideBar.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets, dummyUserData, ownerMenuLinks } from '../../assets/assets'
+import { NavLink, useLocation } from 'react-router-dom';
 
 const SideBar = () => {
+    const user = dummyUserData;
+    const location = useLocation();
+    const [image,setImage] = useState('')
+
+    const updateImage = async () => {
+        user.image = URL.createObjectURL(image)
+        setImage('')
+    }
   return (
-    <div>SideBar</div>
+    <div className='relative min-h-screen md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-borderColor text-sm'>
+        <div className='group relative'>
+            <label htmlFor='image'>
+                <img src={image ? URL.createObjectURL(image) : user?.image} alt='' />
+                <input type='file' id='image' accept='image/*' hidden onChange={(e)=>setImage(e.target.files[0])}/>
+
+                <div className='absolute hidden top-0 right-0 left-0 bottom-0 bg-black/10 rounded-full group-hover:flex items-center justify-center cursor-pointer'>
+                    <img  src={assets.edit_icon}/>
+                </div>
+            </label>
+        </div>
+        {image && (
+            <button className='absolute top-0 right-0 flex p-2 gap-1 bg-blue-800/10 text-blue-800 cursor-pointer '>Save <img src={assets.check_icon} width={13} alt='' onClick={updateImage}/> </button>
+        )}
+        <p className='mt-2 text-base max-md:hidden'>{user?.name}</p>
+        <div className='w-full'>
+            {ownerMenuLinks.map((link, index)=> (
+                <NavLink key={index} to={link.path} className={`relative flex items-center 
+                 gap-2 w-full py-3 pl-4 first:mt-6 ${link.path == location.pathname ? 'bg-blue-800' : 'text-gray-600'}`}>
+                    <img src={link.icon}/>
+                    <span className='max-md:hidden'>{link.name}</span>
+                    <div className={`${link.path === location.pathname && 'bg-blue-800'} w-1.5 h-8 rounded-1 right-0 absolute`}></div>
+                </NavLink>
+            ))}
+        </div>
+    </div>
   )
 }
 
